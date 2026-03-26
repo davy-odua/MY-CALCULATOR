a5dcacdc876f8fc7d1b500552b1df0b383c84718
Davy-odua <davyodua@gmail.com>
2026-03-26 07:54:49 +0300

    calculator

--- mycalculator.py
+++ mycalculator.py
@@ -12,10 +12,14 @@ print("Advanced Operators: (log, sqrt) ")
 calculating = True
 while calculating:
     # Binary operator
-    operat_or = input("Enter the operator: ")
+    operat_or = input("Enter the operator: ").lower()
     if operat_or in ['+', '-', '*', '/', '^', '%']:
-        first_numb = float(input("Enter the first number: "))
-        second_numb = float(input("Enter the second number: "))
+        try:
+            first_numb = float(input("Enter the first number: "))
+            second_numb = float(input("Enter the second number: "))
+        except ValueError:
+            print("Please enter a number")
+            continue
 
         if operat_or == '+':
             print(f"The answer is {first_numb + second_numb}")
@@ -24,17 +28,25 @@ while calculating:
         elif operat_or == '*':
             print(f"The answer is {first_numb * second_numb}")
         elif operat_or == '/':
-            print(f"The answer is {first_numb / second_numb}")
+            if second_numb == 0:
+                print(f"Zero cannot be divided")
+            else:
+                print(f"The answer is {first_numb / second_numb}")
         elif operat_or == '^':
             print(f"The answer is {first_numb ** second_numb}")
         elif operat_or == '%':
             print(f"The answer is {first_numb % second_numb}")
+
     # Unary operator
     elif operat_or in ['sqrt', 'log']:
-        numb = float(input("Enter the number: "))
+        try:
+            numb = float(input("Enter the number: "))
+        except ValueError:
+            print("Enter a number")
+            continue
 
         if operat_or == 'sqrt':
-            if numb > 0:
+            if numb >= 0:
                 print(f"The answer is {math.sqrt(numb)}")
             else:
                 print("Cannot calculate the square root of a negative number")
@@ -42,26 +54,12 @@ while calculating:
             if numb > 0:
                 print(f"The answer is {math.log10(numb)}")
             else:
-                print("Cannot calculate the square root of a negative number")
+                print("Cannot calculate the log of a zero or negative number")
 
     # Invalid operator
     else:
         print("Invalid operator")
 
-    continu_e = input("Continue:y/n) ")
+    continu_e = input("Continue (y/n): ").lower()
     if continu_e == "n":
         break
-
-
-
-
-
-
-
-
-
-
-
-
-
-
